f5a25a1c3a00489d076363a218c6c03bbe339b3b
sreenivasyanamala <sreenivasjava2@gmai.com>
2020-07-07 18:01:48 +0530

    initial commit

---
+++ order-service-api-gateway/src/test/java/com/eatza/order/orderserviceapigateway/OrderServiceApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eatza.order.orderserviceapigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
